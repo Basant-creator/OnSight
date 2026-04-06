b2753a9e43ce09fa5ea69994d9aaaf2fd904e960
Basant Bhushan <basantbhushan89@gmail.com>
2026-04-06 23:02:32 +0530

    Connecting Proper DB rather than LocalDB

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,15 @@
 const express = require("express")
 const mongoose = require("mongoose")
 const cors = require("cors")
-
+const path = require("path")
+require("dotenv").config({ path: path.resolve(__dirname, '../.env') })
 const app = express()
 
 app.use(cors())
 app.use(express.json())
 
-mongoose.connect("mongodb://127.0.0.1:27017/exam_platform")
+const mongoURI = process.env.MONGO_URI || "mongodb://127.0.0.1:27017/exam_platform"
+mongoose.connect(mongoURI)
 .then(() => console.log("MongoDB Connected"))
 .catch(err => console.log(err))
 
